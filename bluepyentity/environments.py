a7e08365a3cd1bfc5c817f46a02ee74209cce665
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2022-11-15 23:01:01 +0100

    Add store overrides and debug in create_forge

--- bluepyentity/environments.py
+++ bluepyentity/environments.py
@@ -7,6 +7,8 @@ import importlib.resources
 import yaml
 from kgforge.core import KnowledgeGraphForge
 
+from bluepyentity.exceptions import BluepyEntityError
+
 ENVIRONMENTS = {
     "prod": "prod-forge-nexus.yaml",
     "staging": "staging-forge-nexus.yaml",
@@ -18,13 +20,81 @@ def get_environment(env):
     return importlib.resources.path("bluepyentity.data", ENVIRONMENTS[env])
 
 
-def create_forge(environment, token, bucket):
-    """create a kgforge.KnowledgeGraphForge object"""
+def create_forge(environment, token, bucket, store_overrides=None, debug=False):
+    """Create a kgforge.KnowledgeGraphForge object
+
+    Args:
+
+        environment (str): Name of the configuration environment. Example: 'prod'
+
+        token (str): Nexus access token.
+
+        bucket (str):
+            Bucket created by the concatenation of organization and project name.
+            Examples: bbp/atlas, bbp/mmb-point-neuron-framework
+        store_overrides (dict):
+            Optional dictionary with overrides for the Store configuration.
+
+            Allowed store_overrides:
+                name: A class name of a store
+                endpoint: A URL. Example: https://bbp.epfl.ch/nexus/v1
+                searchendpoints:
+                 <querytype>: A query paradigm supported by configured store (e.g. sparql)
+                   endpoint: An IRI of a query endpoint.
+                params:
+                   <Store method>: <e.g. register, tag, ...>
+                       param: <http query param value to use for the Store method>
+                versioned_id_template: A string template using 'x' to access resource fields.
+                file_resource_mapping: An Hjson string, a file path, or an URL.
+        debug (bool): If True debug mode is enabled.
+
+    Returns:
+        A KnowledgeGraphForge instance.
+
+    Raises:
+        BluepyEntityError:
+            If `store_overrides` keys are different than the specified ones above.
+
+    Examples:
+        >>> forge = create_forge('prod', token, 'bbp/atlas')
+        >>> forge = create_forge(environment="prod", token=token, bucket="bbp/atlas",
+            store_overrides={
+                "searchendpoints": {
+                    "sparql": {
+                        "endpoint": "https://bbp.epfl.ch/neurosciencegraph/data/views/aggreg-sp/dataset"
+                    },
+                    "elastic": {
+                        "endpoint": "https://bbp.epfl.ch/neurosciencegraph/data/views/aggreg-es/dataset",
+                        "mapping": "https://bbp.epfl.ch/neurosciencegraph/data/views/es/dataset",
+                        "default_str_keyword_field": "keyword",
+                    },
+                },
+            },
+            debug=True)
+    """
+    forge_kwargs = {"debug": debug}
+    if store_overrides:
+        allowed_keys = {
+            "endpoint",
+            "searchendpoints",
+            "params",
+            "versiond_id_template",
+            "file_resource_mapping",
+        }
+        if not all(key in allowed_keys for key in store_overrides):
+            raise BluepyEntityError(
+                f"Unsupported 'store_overrisdes' keys. "
+                f"Supported: {allowed_keys}."
+                f"Encountered: {store_overrides}."
+            )
+        forge_kwargs.update(store_overrides)
+
     with get_environment(environment) as env:
         forge = KnowledgeGraphForge(
             str(env.absolute()),
             token=token,
             bucket=bucket,
+            **forge_kwargs,
         )
         return forge
 
---
+++ tests/test_environments.py
@@ -0,0 +1,14 @@
+import pytest
+from bluepyentity.exceptions import BluepyEntityError
+from bluepyentity import environments as tested
+
+
+def test_create_forge__raises():
+
+    with pytest.raises(BluepyEntityError):
+        tested.create_forge("prod", None, None, store_overrides={"John": "Beatle"})
+
+    with pytest.raises(BluepyEntityError):
+        tested.create_forge(
+            "prod", None, None, store_overrides={"searchendpoints": {}, "John": "Beatle"}
+        )
