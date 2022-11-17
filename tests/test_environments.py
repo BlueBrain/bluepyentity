ee7b4545f09547cc70666d830ed0a730a7cce241
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2022-11-17 12:32:59 +0100

    Add match to check error

--- tests/test_environments.py
+++ tests/test_environments.py
@@ -1,3 +1,4 @@
+import re
 import pytest
 from bluepyentity.exceptions import BluepyEntityError
 from bluepyentity import environments as tested
@@ -5,10 +6,10 @@ from bluepyentity import environments as tested
 
 def test_create_forge__raises():
 
-    with pytest.raises(BluepyEntityError):
+    with pytest.raises(BluepyEntityError, match="Unsupported 'store_overrides' keys."):
         tested.create_forge("prod", None, None, store_overrides={"John": "Beatle"})
 
-    with pytest.raises(BluepyEntityError):
+    with pytest.raises(BluepyEntityError, match="Unsupported 'store_overrides' keys."):
         tested.create_forge(
             "prod", None, None, store_overrides={"searchendpoints": {}, "John": "Beatle"}
         )
