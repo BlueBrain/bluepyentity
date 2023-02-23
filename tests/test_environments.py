ec93c0420f7806fa49b4b083583a54ff8070a7a5
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2023-02-23 10:19:44 +0100

    Make latest black format changes (#38)

--- tests/test_environments.py
+++ tests/test_environments.py
@@ -5,7 +5,6 @@ from bluepyentity import environments as tested
 
 
 def test_create_forge__raises():
-
     with pytest.raises(BluepyEntityError, match="Unsupported 'store_overrides' keys."):
         tested.create_forge("prod", None, None, store_overrides={"John": "Beatle"})
 
